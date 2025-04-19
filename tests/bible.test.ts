532b8bea3ae6427f73e5bba7a435ef4a8e2b7f2b
Carlos Costa <carllos.nc@gmail.com>
2025-04-19 17:00:12 -0300

    Refactor tests to remove Bible Versão Católica and unnecessary language checks

--- tests/bible.test.ts
+++ tests/bible.test.ts
@@ -1,18 +1,11 @@
 import { expect, test } from 'bun:test'
 import BibleKingJames from '../output/json/pt-BR/bible-king-james.json'
-import BibleVersaoCatolica from '../output/json/pt-BR/bible-versao-catolica.json'
 import type { Bible } from '../src/types'
 
 test("Bible King James", () => {
   const bible: Bible = BibleKingJames
 
-  expect(bible.lang).toBe('pt-BR')
-  expect(bible.books.length).toBe(66)
-})
+  console.log(bible.books[0].chapters[0][0])
 
-test("Bible Versão Católica", () => {
-  const bible: Bible = BibleVersaoCatolica
-
-  expect(bible.lang).toBe('pt-BR')
   expect(bible.books.length).toBe(66)
 })
