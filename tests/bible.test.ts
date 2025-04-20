eec63dd54d84c198d2e65e5ed90581c9b81dec15
Carlos Costa <carllos.nc@gmail.com>
2025-04-20 07:40:54 -0300

    Update test descriptions and add Gzip test for Bible King James

--- tests/bible.test.ts
+++ tests/bible.test.ts
@@ -1,9 +1,19 @@
 import { expect, test } from 'bun:test'
 import BibleKingJames from '../output/json/pt-BR/bible-king-james.json'
 import type { Bible } from '../src/types'
+import { file } from 'bun'
 
-test("Bible King James", () => {
+test("Bible King James JSON", () => {
   const bible: Bible = BibleKingJames
+
+  expect(bible.books.length).toBe(66)
+})
+
+test("Bible King James Gzip", async () => {
+  const compressedData = await file('./output/gzip/pt-BR/bible-king-james.gz').arrayBuffer()
+  const decompressedData = Bun.gunzipSync(compressedData)
+  const decompressedDataString = new TextDecoder().decode(decompressedData)
+  const bible: Bible = JSON.parse(decompressedDataString)
 
   expect(bible.books.length).toBe(66)
 })
