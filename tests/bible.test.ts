c08dce413c1e9c1a3f7cac03e280b3ca72f52164
Carlos Costa <carllos.nc@gmail.com>
2025-04-19 21:39:17 -0300

    Remove debug log from Bible King James test

--- tests/bible.test.ts
+++ tests/bible.test.ts
@@ -5,7 +5,5 @@ import type { Bible } from '../src/types'
 test("Bible King James", () => {
   const bible: Bible = BibleKingJames
 
-  console.log(bible.books[0].chapters[0][0])
-
   expect(bible.books.length).toBe(66)
 })
