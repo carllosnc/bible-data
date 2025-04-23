835ace00ab1120caca7110184548ecc468bcc02e
Carlos Costa <carllos.nc@gmail.com>
2025-04-23 19:49:18 -0300

    Add SQLite test for Bible King James to validate database entries

--- tests/bible.test.ts
+++ tests/bible.test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from 'bun:test'
 import BibleKingJames from '../output/json/pt-BR/bible-king-james.json'
 import type { Bible } from '../src/types'
+import Database from 'bun:sqlite'
 import { file } from 'bun'
 
 test("Bible King James JSON", () => {
@@ -17,3 +18,11 @@ test("Bible King James Gzip", async () => {
 
   expect(bible.books.length).toBe(66)
 })
+
+test("Bible King James SQLite", async () => {
+  const db = new Database('./output/sqlite/pt-BR/bible-king-james.sqlite')
+  const books = db.prepare('SELECT * FROM books').all()
+
+  expect(books.length).toBe(66)
+})
+
